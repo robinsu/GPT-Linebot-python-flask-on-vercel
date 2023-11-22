0bb8afa6aa43bca25a33707c0c41ae6d65ee9896
ROBIN SU <robin.rsu@gmail.com>
2023-11-23 00:31:26 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -28,6 +28,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    print(f"callback {body}")
     # handle webhook body
     try:
         line_handler.handle(body, signature)
